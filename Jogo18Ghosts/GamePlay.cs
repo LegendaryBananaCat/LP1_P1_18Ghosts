716877de014ac91f48281c8b59572b9a73e0d5a1
LegendaryBananaCat <joana.f.silva.99@gmail.com>
2019-04-26 17:06:52 +0100

    Test_2

--- Jogo18Ghosts/GamePlay.cs
+++ Jogo18Ghosts/GamePlay.cs
@@ -2,7 +2,120 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Jogo18Ghosts
+{
+
+    public abstract class Players
+    {
+        public string Name { get; set; }
+        public abstract GameAction Act();
+    }
+
+    public class PlayerRed : Players
+    {
+        public override GameAction Act()
+        {
+            return GameAction.Red;
+        }
+    }
+
+    public class PlayerYellow : Players
+    {
+        public override GameAction Act()
+        {
+            return GameAction.Yellow;
+        }
+    }
+
+    public class PlayerBlue : Players
+    {
+        public override GameAction Act()
+        {
+            return GameAction.Blue;
+        }
+    }
+
+    public enum GameAction
+    {
+        Blue,
+        Yellow,
+        Red
+    }
+
+    public class Battle
+    {
+        private readonly Players _player1;
+        private readonly Players _player2;
+
+        public Battle(Players player1, Players player2)
+        {
+            this._player1 = player1;
+            this._player2 = player2;
+        }
+
+        public Players PlayMatchUp()
+        {
+
+            var result = WinningHand(_player1.Act(), _player2.Act());
+
+            if (_player1.Act() == result)
+            {
+                return _player1;
+            }
+
+            if (_player2.Act() == result)
+            {
+                return _player2;
+            }
+
+            return null;
+        }
+        private GameAction? WinningHand(GameAction p1, GameAction p2)
+        {
+            //P1 wins with yellow
+            if (p1 == GameAction.Yellow && p2 == GameAction.Red)
+            {
+                return GameAction.Yellow;
+            }
+
+            //P2 wins with blue
+            if (p1 == GameAction.Yellow && p2 == GameAction.Blue)
+            {
+                return GameAction.Blue;
+            }
+
+            //P1 wins with blue
+            if (p1 == GameAction.Blue && p2 == GameAction.Yellow)
+            {
+                return GameAction.Blue;
+            }
+
+            //P2 wins with red
+            if (p1 == GameAction.Blue && p2 == GameAction.Red)
+            {
+                return GameAction.Red;
+            }
+
+            //P2 wins with yellow
+            if (p1 == GameAction.Red && p2 == GameAction.Yellow)
+            {
+                return GameAction.Yellow;
+            }
+
+            //P1 wins with Red
+            if (p1 == GameAction.Red && p2 == GameAction.Blue)
+            {
+                return GameAction.Red;
+            }
+
+            return null;
+        }
+
+
+    }/
+}
+/*namespace Jogo18Ghosts
 {
     class GamePlay
     {
@@ -11,5 +124,5 @@ namespace Jogo18Ghosts
         // ----------> Constructors <---------- //
 
         // ----------> Functions <---------- //
-    }
-}
+    
+}*/
--- Jogo18Ghosts/Program.cs
+++ Jogo18Ghosts/Program.cs
@@ -2,11 +2,37 @@
 
 namespace Jogo18Ghosts
 {
-    class Program
+    class GamePlay
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("HEEEEEEEEEEEEEEEEEEEE");
+            Console.WriteLine("Tell me your names Player 1, Player 2.");
+
+            var player1 = new PlayerYellow()
+            {
+                Name = Console.ReadLine()
+
+            };
+
+            var player2 = new PlayerBlue()
+            {
+                Name = Console.ReadLine()
+            };
+
+            var winner = new Battle(player1, player2).PlayMatchUp();
+
+            if (winner == null)
+            {
+                Console.WriteLine("You cannot fight him.");
+            }
+            else
+            {
+                Console.WriteLine("The Winner of this battle is {0}", winner.Name);
+            }
+
+            Console.ReadKey();
         }
+
     }
 }
+
